6407dc438c3fc82c97960759ab9137b8410b8919
arush <arush@snakebite.com>
2018-02-24 20:13:23 +0530

    upload

---
+++ 1.1/1.1.5.js
@@ -0,0 +1,8 @@
+//Find if string starts and ends with another given string
+var a = "konfinityMemberninja";
+var c  = Number(a.search(prompt()));
+if (c > 0 ){
+   console.log('yes');
+}else{
+  console.log('no');
+}

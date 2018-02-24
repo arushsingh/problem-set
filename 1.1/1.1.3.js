1f9e8602d2570509927028b5096e4664754d932e
arush <arush@snakebite.com>
2018-02-24 19:49:43 +0530

    update assignment

--- 1.1/1.1.3.js
+++ 1.1/1.1.3.js
@@ -1,1 +1,8 @@
 //Print the sum of infinite series +1, -1, +1, -1 ...
+var a = Number(prompt('Enter a number'));
+var sum  = 0;
+if ( a % 2 === 0 ){
+  console.log('the result is 0');
+}else {
+  console.log('the result is 1');
+}
